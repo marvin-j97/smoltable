436a96ac1d9c6b5b04891241acb57b5004e412ab
marvin-j97 <marvin.janke.97@gmail.com>
2024-01-25 17:56:04 +0100

    fix docs base url

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	base: "/smoltable",
 	integrations: [
 		starlight({
 			title: 'Smoltable',
